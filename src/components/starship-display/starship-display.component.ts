6abb1ef585101a34cc26ad2b9a38953ca0b5ad36
Cole Thacker <cthacker@opentext.com>
2025-02-04 09:21:06 -0500

    changing starship display to be more reactive

--- src/components/starship-display/starship-display.component.ts
+++ src/components/starship-display/starship-display.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, input, model, output, signal, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, ElementRef, input, model, output, signal, viewChild } from '@angular/core';
 import { StarShip } from '../../services/star-wars.service';
 
 @Component({
@@ -14,32 +14,32 @@ import { StarShip } from '../../services/star-wars.service';
 				<input #nameInput [value]="currentStarShip.name" (input)="updateStarShipName(nameInput.value)"/>
 			</div>
 		} @else {
-			<div (mouseenter)="showTooltip($event, 'Starship name')" (mouseleave)="hideTooltip()">
+			<div (mouseenter)="setTooltip($event, 'Starship name')" (mouseleave)="hideTooltip()">
 				Name: {{currentStarShip.name}}
 			</div>
 		}
 
-		<div (mouseenter)="showTooltip($event, 'Starship model')" (mouseleave)="hideTooltip()">
+		<div (mouseenter)="setTooltip($event, 'Starship model')" (mouseleave)="hideTooltip()">
 			Model: {{currentStarShip.model}}
 		</div>
 
-		<div (mouseenter)="showTooltip($event, 'Starship maker')" (mouseleave)="hideTooltip()">
+		<div (mouseenter)="setTooltip($event, 'Starship maker')" (mouseleave)="hideTooltip()">
 			Manufacturer: {{currentStarShip.manufacturer}}
 		</div>
 
-		<div (mouseenter)="showTooltip($event, 'Hyperdrive rating')" (mouseleave)="hideTooltip()">
+		<div (mouseenter)="setTooltip($event, 'Hyperdrive rating')" (mouseleave)="hideTooltip()">
 			Hyperdrive Rating: {{currentStarShip.hyperdrive_rating}}
 		</div>
 
-		<div (mouseenter)="showTooltip($event, 'Crew size')" (mouseleave)="hideTooltip()">
+		<div (mouseenter)="setTooltip($event, 'Crew size')" (mouseleave)="hideTooltip()">
 			Crew Size: {{currentStarShip.crew}}
 		</div>
 
-		<div (mouseenter)="showTooltip($event, 'Starship length')" (mouseleave)="hideTooltip()">
+		<div (mouseenter)="setTooltip($event, 'Starship length')" (mouseleave)="hideTooltip()">
 			Length: {{currentStarShip.length}}
 		</div>
 
-		<div (mouseenter)="showTooltip($event, 'Cost in credits')" (mouseleave)="hideTooltip()">
+		<div (mouseenter)="setTooltip($event, 'Cost in credits')" (mouseleave)="hideTooltip()">
 			Cost: {{currentStarShip.cost_in_credits}}
 		</div>
 
@@ -56,21 +56,34 @@ export class StarshipDisplayComponent {
 
 	readonly likeStarShip = output<void>();
 
-	readonly displayTooltip = signal<boolean>(false);
+	readonly tooltipInfo = signal<{text: string, left: string, top: string} | null>(null);
+	readonly displayTooltip = computed<boolean>(() => this.tooltipInfo() !== null);
 
 	readonly tooltip = viewChild.required<ElementRef<HTMLElement>>('tooltip');
 
-	showTooltip(event: MouseEvent, tooltipText: string) {
-		this.displayTooltip.set(true);
-		const tooltipDiv = this.tooltip().nativeElement;
+	constructor() {
+		effect(() => {
+			const tooltipInfo = this.tooltipInfo();
+			if (tooltipInfo !== null) {
+				const tooltipDiv = this.tooltip().nativeElement;
 
-		tooltipDiv.textContent = tooltipText;
-		tooltipDiv.style.left = event.clientX +'px';
-		tooltipDiv.style.top = event.clientY + 'px';
+				tooltipDiv.textContent = tooltipInfo.text;
+				tooltipDiv.style.left = tooltipInfo.left;
+				tooltipDiv.style.top = tooltipInfo.top;
+			}
+		});
+	}
+
+	setTooltip(event: MouseEvent, text: string) {
+		this.tooltipInfo.set({
+			text: text,
+			left: event.clientX + 'px',
+			top: event.clientY + 'px'
+		});
 	}
 
 	hideTooltip(): void {
-		this.displayTooltip.set(false);
+		this.tooltipInfo.set(null);
 	}
 
 	updateStarShipName(newName: string) {
